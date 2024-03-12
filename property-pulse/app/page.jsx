c0d7c77aa7b6ac815e78693d1086dcc2bbc18c8b
MrMoovf <anderskozuch1998@gmail.com>
2024-03-12 15:58:48 +0100

    d

--- property-pulse/app/page.jsx
+++ property-pulse/app/page.jsx
@@ -10,6 +10,7 @@ const HomePage = async () => {
 			<InfoBoxes/>
 			<HomeProperties/>
 			<Footer/>
+			<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4qMBuq9LoKQb7thBpQltiN?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 		</>
     )
 }
---
+++ property-pulse/app/test/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div>
+      <iframe className='w-96' src="https://open.spotify.com/embed/track/4qMBuq9LoKQb7thBpQltiN?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  )
+}
+
+export default TestPage
